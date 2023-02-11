a57c007693d77d9868555ac8f5c78dfe0930426c
ronax <122752743+thatsronax@users.noreply.github.com>
2023-02-11 21:07:14 +0300

    small update
    added blank "Settings window", corners are now more rounded

--- js/windows_actions.js
+++ js/windows_actions.js
@@ -23,3 +23,16 @@ function open_window_justice() {
     document.getElementById("appwindow_justice").classList.remove("appwindow_hidden");
     document.getElementById("pgtitle").innerHTML = "D3adC0re OS - Just Ice";
 }
+
+//Open/Close Settings
+function close_window_settings() {
+    document.getElementById("appwindow_settings").classList.add("appwindow_hidden");
+    document.getElementById("appwindow_settings").classList.remove("appwindow_shown");
+    document.getElementById("pgtitle").innerHTML = "D3adC0re OS";
+}
+function open_window_settings() {
+    hidesmenu()
+    document.getElementById("appwindow_settings").classList.add("appwindow_shown");
+    document.getElementById("appwindow_settings").classList.remove("appwindow_hidden");
+    document.getElementById("pgtitle").innerHTML = "D3adC0re OS - Settings";
+}
--- js/windows_drag.js
+++ js/windows_drag.js
@@ -8,22 +8,20 @@ function window_draggable_terminal() {
     $('#appwindow_terminal').on( 'drag', function( event, ui ) {} );
     $('#appwindow_terminal').draggable({
         drag: function( event, ui ) {
-
             ui.position.left = Math.max( ui.position.left, -28 );
             ui.position.top = Math.max( ui.position.top, -20 );
-            
         }
     });
 }
-
 function topbar_enter() {
     $('#appwindow_terminal').draggable( "enable" );
 }
 function topbar_leave() {
     $('#appwindow_terminal').draggable( "disable" );
 }
-
 document.addEventListener("DOMContentLoaded", window_draggable_terminal);
+
+
 
 //Just Ice drag
 function window_draggable_justice() {
@@ -35,19 +33,40 @@ function window_draggable_justice() {
     $('#appwindow_justice').on( 'drag', function( event, ui ) {} );
     $('#appwindow_justice').draggable({
         drag: function( event, ui ) {
-
             ui.position.left = Math.max( ui.position.left, -38 );
             ui.position.top = Math.max( ui.position.top, -10 );
-            
         }
     });
 }
-
 function topbar_enter_justice() {
     $('#appwindow_justice').draggable( "enable" );
 }
 function topbar_leave_justice() {
     $('#appwindow_justice').draggable( "disable" );
 }
-
 document.addEventListener("DOMContentLoaded", window_draggable_justice);
+
+
+
+//Just Ice drag
+function window_draggable_settings() {
+    $('#appwindow_settings').draggable( );
+    $('#appwindow_settings').draggable( 'option', 'scroll', false );
+    $('#appwindow_settings').draggable( 'option', 'snap', false );
+    $('#appwindow_settings').draggable( "option", "cursor", "move" );
+    $('#appwindow_settings').draggable( 'disable' );
+    $('#appwindow_settings').on( 'drag', function( event, ui ) {} );
+    $('#appwindow_settings').draggable({
+        drag: function( event, ui ) {
+            ui.position.left = Math.max( ui.position.left, -38 );
+            ui.position.top = Math.max( ui.position.top, -10 );
+        }
+    });
+}
+function topbar_enter_settings() {
+    $('#appwindow_settings').draggable( "enable" );
+}
+function topbar_leave_settings() {
+    $('#appwindow_settings').draggable( "disable" );
+}
+document.addEventListener("DOMContentLoaded", window_draggable_settings);
