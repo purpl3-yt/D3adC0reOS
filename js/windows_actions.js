50f4ea43813607178c353b13c90860a128e2f5d1
purpl3-yt <remezdevid2000@gmail.com>
2023-02-11 16:19:46 +0200

    add files

---
+++ js/windows_actions.js
@@ -0,0 +1,25 @@
+//Open/Close terminal
+function close_window_terminal() {
+    document.getElementById("appwindow_terminal").classList.add("appwindow_hidden");
+    document.getElementById("appwindow_terminal").classList.remove("appwindow_shown");
+    document.getElementById("pgtitle").innerHTML = "D3adC0re OS";
+}
+function open_window_terminal() {
+    hidesmenu()
+    document.getElementById("appwindow_terminal").classList.add("appwindow_shown");
+    document.getElementById("appwindow_terminal").classList.remove("appwindow_hidden");
+    document.getElementById("pgtitle").innerHTML = "D3adC0re OS - T3rm1nal";
+}
+
+//Open/Close Just Ice
+function close_window_justice() {
+    document.getElementById("appwindow_justice").classList.add("appwindow_hidden");
+    document.getElementById("appwindow_justice").classList.remove("appwindow_shown");
+    document.getElementById("pgtitle").innerHTML = "D3adC0re OS";
+}
+function open_window_justice() {
+    hidesmenu()
+    document.getElementById("appwindow_justice").classList.add("appwindow_shown");
+    document.getElementById("appwindow_justice").classList.remove("appwindow_hidden");
+    document.getElementById("pgtitle").innerHTML = "D3adC0re OS - Just Ice";
+}
